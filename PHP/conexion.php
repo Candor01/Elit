a50b4fb973ce2442db552c71d7aa41a7ab903868
JK <juanksilva9@hotmail.com>
2020-10-21 17:10:51 -0500

    conexion,funciones

---
+++ PHP/conexion.php
@@ -0,0 +1,9 @@
+<?php
+$conn=mysqli_connect("localhost","root","","peliculas");
+          // Check connection
+          if (mysqli_connect_errno())
+            {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            }
+            mysqli_set_charset($conn,"utf8");
+?>
