2b0554b35600819066ab87ffca7a1e5e84d6f0dd
ElkinC <juanchocastellanos07@gmail.com>
2020-10-22 05:36:31 -0500

    Busqueda base

---
+++ busqueda.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Elit</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Baumans&family=Quicksand&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="Estilos/estilos.css" >
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+  </head>
+  <body>
+    <header>
+      <div class="container">
+<div class="row">
+  <div class="col-xs-12 col-sm-12 col-md-6">
+        <h1>Elit</h1></div>
+        <div class="col-xs-12 col-sm-12 col-md-6">
+        <form class="form-inline md-form mr-auto mb-4">
+  <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+  <button class="btn aqua-gradient btn-rounded btn-sm my-0" type="submit" style="background-color: white;">Search</button>
+</form>
+</div>
+</div>
+      </div>
+    </header>
+    <div class="container">
+
+    
+    </div>
+
+ 
+    <footer>
+     
+    </footer>
+    
+    <!--<script src="scripts.js"></script>-->
+    <script></script>
+  
+  </body>
+</html>
